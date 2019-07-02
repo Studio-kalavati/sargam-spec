e252bdff8e165a4155811a3e5ed865f258c32078
shark8me <kiran.karkera@gmail.com>
2019-07-02 23:18:41 +0800

    lein-release plugin: preparing 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject studiokalavati/sargam-spec "0.1.2"
+(defproject studiokalavati/sargam-spec "0.1.1"
   :description "Clojure and json spec for bhatkhande notation"
   :url "https://github.com/Studio-kalavati/sargam-spec"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :repositories [["releases" {:url "https://repo.clojars.org"
+                            :creds :gpg}]]
   :deploy-repositories [["releases" :clojars]
                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.0"]
