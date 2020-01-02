285550e2bd20f677ee849658dcf26b720749e5cc
shark8me <kiran.karkera@gmail.com>
2020-01-02 23:48:50 +0800

    bump version to 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject studiokalavati/sargam-spec "0.1.5"
+(defproject studiokalavati/sargam-spec "0.1.6-SNAPSHOT"
   :description "Clojure and json spec for bhatkhande notation"
   :url "https://github.com/Studio-kalavati/sargam-spec"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
