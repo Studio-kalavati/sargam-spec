0c003708b97c7d4910db798665c3d9b049606abc
shark8me <kiran.karkera@gmail.com>
2020-01-06 14:00:17 +0800

    trying to deploy with gpg

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repositories [["releases" {:url "https://repo.clojars.org"
-                            :creds :gpg}]]
+                              :username [:gpg]
+                              :password [:gpg]}]]
   :deploy-repositories [["releases" :clojars]
                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.0"]
