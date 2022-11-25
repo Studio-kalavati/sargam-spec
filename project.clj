3a0ba1437872c6dc4da526882692846221c82a64
shark8me <kiran.karkera@gmail.com>
2022-11-25 12:28:59 +0800

    gpg cred format

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repositories [["releases" {:url "https://repo.clojars.org"
-                              :username [:gpg]
-                              :password [:gpg]}]]
+                              :creds :gpg }]]
   :deploy-repositories [["releases" :clojars]
                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.0"]
