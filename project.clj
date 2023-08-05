9b2c17d33d07abba61f54ffa75afe53af68c931e
shark8me <kiran.karkera@gmail.com>
2023-08-05 11:32:48 +0200

    fix maven url

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repositories [
-                 ["github" {:url "https://maven.pkg.github.com/<githubusername>/<repo>" 
+                 ["github" {:url "https://maven.pkg.github.com/studio-kalavati/sargam-spec"
                             :username [:env/GITHUB_USER] :password [:env/GITHUB_TOKEN]}]
                  ["releases" {:url "https://repo.clojars.org"
                               :username :env
