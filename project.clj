ead7b357acc5b545e6fa1a5598938bdd82532e29
shark8me <kiran.karkera@gmail.com>
2023-08-05 11:28:59 +0200

    trying out GH publish

--- project.clj
+++ project.clj
@@ -3,12 +3,14 @@
   :url "https://github.com/Studio-kalavati/sargam-spec"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :repositories [["releases" {:url "https://repo.clojars.org"
+  :repositories [
+                 ["github" {:url "https://maven.pkg.github.com/<githubusername>/<repo>" 
+                            :username [:env/GITHUB_USER] :password [:env/GITHUB_TOKEN]}]
+                 ["releases" {:url "https://repo.clojars.org"
                               :username :env
                               :password :env
                               }]]
-  :deploy-repositories [["releases" :clojars]
-                       ["snapshots" :clojars]]
+  :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"] ]
   :repl-options {:init-ns sargam-spec.core})
